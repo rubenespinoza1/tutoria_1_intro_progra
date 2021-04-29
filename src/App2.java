3265308e75d23feab955af48d38f199c556bc2cc
Ruben Espinoza Osses <r.espinoza05@ufromail.cl>
2021-04-29 12:54:14 -0400

    Problema determinar numero mayor en app 2

--- src/App2.java
+++ src/App2.java
@@ -5,18 +5,19 @@ public class App2 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");//Escribir "Hello, World!";
         Scanner intro = new Scanner(System.in);
-        int numero1 = intro.nextInt();//numero1 = 1;
-        int numero2 = intro.nextInt();//numero2 = 3;
+        int numeroMayor = 0;
+        int contador = 0;
 
-
-        if (numero1 < numero2){// Si (numero1 < numero2)
-            System.out.println("numero1 es menor");
-        }else if (numero1 > numero2){//sino(numero2 < numero1)
-            System.out.println("numero2 es menor");
-        }else{//EnOtroCaso -> ==
-            System.out.println("numero1 es igual a numero 2");
+        while(contador < 10){
+            System.out.println("Ingrese un numero porfavor");
+            int numeroUsuario = intro.nextInt();
+            if (numeroUsuario >= numeroMayor){
+                numeroMayor = numeroUsuario;
+            }
+            contador++;
         }
 
+        System.out.println("El numero mayor es: " + numeroMayor);
 
     }
     
