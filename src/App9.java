cb2f5d8fd73b3859b00e627e5d42ffbe3618c04e
Ruben Espinoza Osses <r.espinoza05@ufromail.cl>
2021-05-11 10:38:01 -0400

    Se agrega ejemplo random

--- src/App9.java
+++ src/App9.java
@@ -4,8 +4,11 @@ public class App9 {
     
     public static void main(String[] args){
         Random rand = new Random();
+        int nroRandom = rand.nextInt(31)+60; // numeroMaximoAObtener=numero_en_parentesis + numero_exterior
+        System.out.println(nroRandom);
         int[] numeros = new int[10];
 
+        
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = rand.nextInt(91)+1;
         }
@@ -13,7 +16,6 @@ public class App9 {
         for (int i = 0; i < numeros.length; i++){
             System.out.println(numeros[i]);
         }
-
     }
 
 }
