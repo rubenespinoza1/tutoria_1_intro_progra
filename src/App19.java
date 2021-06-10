60273cf857708b107f00382e17c89f176c402c52
Ruben Espinoza Osses <r.espinoza05@ufromail.cl>
2021-06-10 11:25:12 -0400

    Se añade introduccion a metodos

---
+++ src/App19.java
@@ -0,0 +1,22 @@
+public class App19 {
+
+    public static void main(String[] args) {
+        System.out.println(esPrimo(90));
+    }
+
+    public static boolean esPrimo(int numero){
+        int divPerfectas = 0;
+        for (int i = 1; i <= numero; i++){
+            if (numero % i == 0){
+                divPerfectas++;
+            }
+        }
+        /*if (divPerfectas == 2){
+            return true;
+        }
+        return false;*/
+
+        return divPerfectas == 2;
+    }
+
+}
