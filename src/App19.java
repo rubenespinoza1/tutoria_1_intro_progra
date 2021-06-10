f2359a667ab4eb91065e94e22217183dc2dd3627
Ruben Espinoza Osses <r.espinoza05@ufromail.cl>
2021-06-10 11:46:01 -0400

    Se agrega introduccion a metodos

--- src/App19.java
+++ src/App19.java
@@ -19,4 +19,6 @@ public class App19 {
         return divPerfectas == 2;
     }
 
+
+
 }
---
+++ src/App20.java
@@ -0,0 +1,26 @@
+public class App20 {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 4;
+
+        int c = 4;
+        int d = 8;
+
+        System.out.println(sumarDosNumeros(7, 3));
+
+
+    }
+
+    //multiplicidad tipoRetorno nombreFuncion (parametros)
+
+
+    public static int sumarDosNumeros(int a, int b){
+        int suma = a + b;
+        return suma;
+    }
+
+
+
+}
